5342be39434836ddec9c8a4b9e37e291e968e07c
Azeem Shaik <azeemshaik025@gmail.com>
2024-10-14 21:40:44 +0530

    add build folder to gitignore

--- dist/components/ButtonCompnent.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const jsx_runtime_1 = require("react/jsx-runtime");
-const ButtonComponent = ({ type = 'button', onSubmit, label, disabled = false, style }) => {
-    return ((0, jsx_runtime_1.jsx)("button", { type: type, style: style, onClick: onSubmit, disabled: disabled, className: `mt-4 p-2 rounded w-full ${disabled ? 'bg-gray-400' : 'bg-blue-500 hover:bg-blue-700'} text-white`, children: label }));
-};
-exports.default = ButtonComponent;
